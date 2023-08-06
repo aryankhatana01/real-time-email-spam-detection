c3c51144869d3966f6892d53ef2093782c8341b6
aryankhatana01 <aryankhatana01@gmail.com>
2023-08-06 14:12:47 +0530

    separated inference from training

---
+++ inference/config.py
@@ -0,0 +1,9 @@
+import torch
+from transformers import BertTokenizer
+
+PRE_TRAINED_MODEL_NAME = 'bert-base-cased'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
+EPOCHS = 10
+MAX_LEN = 512
+BATCH_SIZE = 64

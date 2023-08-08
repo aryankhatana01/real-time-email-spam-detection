a249903f2db3930e8aa9fcdd47695d32f5e7cf24
aryankhatana01 <aryankhatana01@gmail.com>
2023-08-08 21:20:55 +0530

    fixed copilot fuckup

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 KAFKA_BROKER = "localhost:29092"
 NUM_PARTITIONS = 3
 EMAILS_TOPIC = "emails"
-EMAILS_GROUP_ID = "emails_group"
+EMAILS_GROUP_ID = "emails"
 
 PREDICTIONS_TOPIC = "predictions"
-PREDICTIONS_GROUP_ID = "predictions_group"
+PREDICTIONS_GROUP_ID = "predictions"

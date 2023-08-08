34c5e78f3913d8b7fdfeccf29152494b7e338d67
aryankhatana01 <aryankhatana01@gmail.com>
2023-08-08 16:27:44 +0530

    created some utils for the pub/sub arch

---
+++ _logging.py
@@ -0,0 +1,34 @@
+import logging
+import colorlog
+
+def get_logger():
+    # Set up the root logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    # Create a color formatter
+    formatter = colorlog.ColoredFormatter(
+        '%(asctime)s - %(log_color)s%(levelname)s - %(message)s',
+        log_colors={
+            'DEBUG': 'cyan',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red,bg_white',
+        },
+        secondary_log_colors={
+            'message': {
+                'ERROR': 'red',
+                'CRITICAL': 'red',
+            }
+        }
+    )
+
+    # Create a console handler and set the formatter
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    # Add the console handler to the root logger
+    logger.addHandler(console_handler)
+
+    return logger
---
+++ config.py
@@ -0,0 +1,6 @@
+KAFKA_BROKER = "localhost:29092"
+EMAILS_TOPIC = "emails"
+EMAILS_GROUP_ID = "emails_group"
+
+PREDICTIONS_TOPIC = "predictions"
+PREDICTIONS_GROUP_ID = "predictions_group"

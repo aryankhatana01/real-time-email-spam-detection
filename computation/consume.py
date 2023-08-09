b02c772e1101e2648d82d550c866942111130004
aryankhatana01 <aryankhatana01@gmail.com>
2023-08-09 11:46:01 +0530

    added data cleanup to the consumer[WORKING FINE]

--- computation/consume.py
+++ computation/consume.py
@@ -7,8 +7,10 @@ from multiprocessing import Process
 from utils import create_consumer, create_producer
 import config
 from _logging import logger
+from preprocesser import convert_to_string_and_cleanup
 
 consumer = create_consumer(topic=config.EMAILS_TOPIC, group_id=config.EMAILS_GROUP_ID)
+producer = create_producer()
 
 while True:
     message = consumer.poll(timeout=50)
@@ -18,4 +20,5 @@ while True:
         logger.error("Consumer error: {}".format(message.error()))
         continue
     record = json.loads(message.value().decode('utf-8'))
+    record = convert_to_string_and_cleanup(record)
     logger.info(f"Message received with ID {record['id']}")

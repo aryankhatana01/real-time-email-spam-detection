dfe79cf5d80f241ed63eb3895a1f81693dc5987f
aryankhatana01 <aryankhatana01@gmail.com>
2023-08-09 16:07:13 +0530

    added map_location=cpu

--- computation/configure_model.py
+++ computation/configure_model.py
@@ -4,6 +4,9 @@ import torch
 
 def get_configured_model():
     model = SpamClassifier(n_classes=2)
-    model.load_state_dict(torch.load(model_cfg.MODEL_PATH))
+    if model_cfg.device == torch.device('cpu'):
+        model.load_state_dict(torch.load(model_cfg.MODEL_PATH, map_location=torch.device('cpu')))
+    else:
+        model.load_state_dict(torch.load(model_cfg.MODEL_PATH))
     model = model.to(model_cfg.device)
     return model
